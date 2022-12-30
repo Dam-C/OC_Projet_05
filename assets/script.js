6328a240cf3bf64087c0252f6c2853d36b935f7e
Dam-C <dams.c128@gmail.com>
2022-12-30 15:58:13 +0100

    JS en cours

--- assets/script.js
+++ assets/script.js
@@ -17,8 +17,14 @@ const slides = [
 	}
 ]
 
-// Modifie le DOM en ajoutant une DIV de class "DOT" pour chaque éléments image présent dans le tableau "slides"
+// Modifie le code pour afficher la première image 
+// ajoute la classe "show"
 
+//let bannerShow = document.querySelector("#banner > .banner__slide");
+//bannerShow.classList.add("banner__slide--show");
+
+// Modifie le DOM en ajoutant une DIV de class "DOT" 
+// pour chaque éléments image présent dans le tableau "slides"
 
 for (let i = 0; i < slides.length; i++) {
 	let leDot = document.createElement("div");
@@ -27,21 +33,26 @@ for (let i = 0; i < slides.length; i++) {
 	lesDots.appendChild(leDot);
 }
 
-
-
+let myDot = document.querySelector("#banner > div.dots > .dot");
+myDot.classList.add("dot_selected");
 
 // générer (i) dot dans [dots]
 // si l'image affichée correspond à l'image [x] de slides
 // remplacer [dot][i] de [dots] par [dot-selected]
 
+let count = 0; // pour naviguer dans les index de slides
+let bannerImgShow = slides[count].image;
+let bannerImg = document.getElementById("bannerImg");
+bannerImg.src = "./assets/images/slideshow/" + bannerImgShow;
 
 document
 	.getElementById("arrowLeft")
 	.addEventListener("click", function(event){
 		event.preventDefault();    
 		event.stopPropagation();
-		document
-			alert("gauche");
+
+//		document
+//			alert("gauche");
 })
 
 document
@@ -49,6 +60,11 @@ document
 	.addEventListener("click", function(event){
 		event.preventDefault();    
 		event.stopPropagation();
-		document
-			alert("droite");
+		if (count > slides.length) {
+			count == 0;
+		} else {
+			count += 1;
+		}
+//		document
+//			alert("droite");
 })
